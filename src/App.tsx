67c59962d5188ae5a2bb16ef36cdd8f679da60d6
Naoto <cafeaulait1992@gmail.com>
2025-11-30 13:47:57 +0900

    キッチンページを追加

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import "./App.scss";
 import Footer from "./layout/Footer";
 import Header from "./layout/Header";
 import AirConditionerPage from "./pages/AirConditionerPage/AirConditionerPage";
+import KitchenPage from "./pages/KitchenPage/KitchenPage";
 import HomePage from "./pages/HomePage/HomePage";
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
             <Routes>
                 <Route path="/" element={<HomePage />} />
                 <Route path="/air-conditioner/*" element={<AirConditionerPage />} />
+                <Route path="/kitchen/*" element={<KitchenPage />} />
                 <Route path="*" element={<HomePage />} />
             </Routes>
             <Footer />
